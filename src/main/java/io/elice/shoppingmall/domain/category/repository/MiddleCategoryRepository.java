28d658cd440c0eedd8069400f22b4fe608d08416
[Cloud]나정균 <dotseven57@gmail.com>
2024-02-27 18:27:54 +0900

    feat: MiddleCategory -> SecondCategory 이름수정(코드리뷰 반영)

--- src/main/java/io/elice/shoppingmall/domain/category/repository/MiddleCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package io.elice.shoppingmall.domain.category.repository;
-
-import io.elice.shoppingmall.domain.category.entity.MiddleCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MiddleCategoryRepository extends JpaRepository<MiddleCategory, Long> {
-}
