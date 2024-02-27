bb06ec762de7d3dd7e43c8032071c801f4d42244
[Cloud]나정균 <dotseven57@gmail.com>
2024-02-27 18:45:41 +0900

    feat: SecondCategory CRUD 기능 구현

---
+++ src/main/java/io/elice/shoppingmall/domain/category/dto/payload/SecondCategoryUpdatePayload.java
@@ -0,0 +1,11 @@
+package io.elice.shoppingmall.domain.category.dto.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SecondCategoryUpdatePayload {
+    private String name;
+    private Long FirstCategoryId;
+}
