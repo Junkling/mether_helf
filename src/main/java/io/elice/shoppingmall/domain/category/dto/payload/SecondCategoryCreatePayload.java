05167fae8fa9a9e7c6f7cb29c1e02222008b8b4b
[Cloud]나정균 <dotseven57@gmail.com>
2024-03-01 19:29:50 +0900

    feat: Category Validation 추가

--- src/main/java/io/elice/shoppingmall/domain/category/dto/payload/SecondCategoryCreatePayload.java
+++ src/main/java/io/elice/shoppingmall/domain/category/dto/payload/SecondCategoryCreatePayload.java
@@ -1,14 +1,15 @@
 package io.elice.shoppingmall.domain.category.dto.payload;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SecondCategoryCreatePayload {
-
+    @NotEmpty
     private String name;
-
+    @NotEmpty
     private Long firstCategoryId;
 
 }
