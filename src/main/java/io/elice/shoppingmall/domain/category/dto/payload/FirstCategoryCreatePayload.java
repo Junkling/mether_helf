2a32319949cc3a48e95ffc3a800296c94d99534c
[Cloud]나정균 <dotseven57@gmail.com>
2024-03-04 23:00:30 +0900

    feat: FirstCategory import 문 정리

--- src/main/java/io/elice/shoppingmall/domain/category/dto/payload/FirstCategoryCreatePayload.java
+++ src/main/java/io/elice/shoppingmall/domain/category/dto/payload/FirstCategoryCreatePayload.java
@@ -1,12 +1,8 @@
 package io.elice.shoppingmall.domain.category.dto.payload;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
 
 
 @Getter
